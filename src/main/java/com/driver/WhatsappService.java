dae7a9becfe43d4cc882694f8fb9b5c7ff4330bb
Rhythin <rhythin13021996@gmail.com>
2023-01-28 17:02:08 +0530

    no bonus features

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -1,31 +1,12 @@
 package com.driver;
 
-import java.util.*;
+import org.springframework.stereotype.Service;
 
-import org.springframework.stereotype.Repository;
+import java.util.Date;
+import java.util.List;
 
-@Repository
-public class WhatsappRepository {
-
-    //Assume that each user belongs to at most one group
-    //You can use the below mentioned hashmaps or delete these and create your own.
-    private HashMap<Group, List<User>> groupUserMap;
-    private HashMap<Group, List<Message>> groupMessageMap;
-    private HashMap<Message, User> senderMap;
-    private HashMap<Group, User> adminMap;
-    private HashSet<String> userMobile;
-    private int customGroupCount;
-    private int messageId;
-
-    public WhatsappRepository(){
-        this.groupMessageMap = new HashMap<Group, List<Message>>();
-        this.groupUserMap = new HashMap<Group, List<User>>();
-        this.senderMap = new HashMap<Message, User>();
-        this.adminMap = new HashMap<Group, User>();
-        this.userMobile = new HashSet<>();
-        this.customGroupCount = 0;
-        this.messageId = 0;
-    }
+@Service
+public class WhatsappService {
 
     WhatsappRepository whatsappRepository=new WhatsappRepository();
 
@@ -61,4 +42,5 @@ public class WhatsappRepository {
     public String findMessage(Date start, Date end, int K) throws Exception{
         return whatsappRepository.findMessage(start, end, K);
     }
+
 }
